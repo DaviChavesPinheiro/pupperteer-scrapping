243a1acc65f416b5b3779b8644054b90a75fafb2
Davi <davicpinheiro123@hotmail.com>
2020-10-05 18:19:47 -0300

    teste 3

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const puppeteer = require('puppeteer');
             '--no-sandbox',
             '--disable-setuid-sandbox',
         ],
-        // headless: false
+        headless: false
     });
     const page = await browser.newPage();
-
-    await page.goto('https://charlotte-services.herokuapp.com/mangas');
+    await page.goto('https://manganero.site/#/manga/642');
+    await page.waitFor('.sc-jrsJCI.sc-iqAbSa.dZkMxf.cYCoKy p')
 
     const chapters = await page.evaluate(() => {
-        const info = document.querySelector("pre").textContent
+        const info = document.querySelector(".sc-jrsJCI.sc-iqAbSa.dZkMxf.cYCoKy p").textContent
         // console.log(document.querySelector("pre"))
+        console.log(info)
         return info
     });
 
