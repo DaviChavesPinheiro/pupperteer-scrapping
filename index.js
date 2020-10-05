14b777db6eeb0112f857735108b48c65bbf0ee4d
Davi <davicpinheiro123@hotmail.com>
2020-10-05 17:53:43 -0300

    Puppeteer

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+    });
+    const page = await browser.newPage();
+
+    await page.goto('https://mangayabu.com/manga/vagabond');
+
+    const chapters = await page.evaluate(() => {
+        const chaptersElements = document.querySelectorAll(".single-chapter a")
+        const chapters = []
+        chaptersElements.forEach(chapterElement => {
+            chapters.push({ title: chapterElement.textContent, url: chapterElement.href })
+        })
+        return chapters.reverse()
+    });
+
+    console.log('chapters:', chapters);
+
+    await browser.close();
+})();
